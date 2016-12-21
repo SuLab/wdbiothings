6954583e52902703bfc0dc7a1baf833c88f9ece3
Greg Stupp <gstupp@scripps.edu>
2016-12-20 16:33:06 -0800

    use v3 mygene. add yeast bot trigger

--- wdbiothings/contrib/mygene/dumper.py
+++ wdbiothings/contrib/mygene/dumper.py
@@ -1,55 +1,38 @@
 import os
 from datetime import datetime
-
+from mygene import MyGeneInfo
 import biothings
 import requests
 from biothings.dataload.dumper import HTTPDumper
 from wdbiothings import config
 from wdbiothings.config import DATA_ARCHIVE_ROOT
+import json
+from dateutil import parser as du
 
 biothings.config_for_app(config)
-"""
-TODO: This is using v2 of mygene api which has a bug that doesn't cap the size limit...
-Not sure if it'll work once I'm requesting 150 microbial genomes also
-"""
-
-class HTTPDumperParams(HTTPDumper):
-    """
-    subclass httpdumper so I can make a GET request with params
-    """
-
-    def download(self, remoteurl, localfile):
-        self.prepare_local_folders(localfile)
-        self.logger.debug("Downloading '%s'" % remoteurl)
-        res = self.client.get(remoteurl, stream=True, params=self.params)
-        fout = open(localfile, 'wb')
-        for chunk in res.iter_content(chunk_size=512 * 1024):
-            if chunk:
-                fout.write(chunk)
-        fout.close()
 
 
-class MyGeneDumper(HTTPDumperParams):
+class MyGeneDumper(HTTPDumper):
     SRC_NAME = "mygene"
     SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
     SCHEDULE = "* 0 * * *"
 
     taxids = "559292,123,10090,9606"
-    params = dict(q="__all__", species=taxids, entrezonly="true", size="1000000",
+    params = dict(q="__all__", species=taxids, entrezonly="true", size="1000",
                   fields="entrezgene,ensembl,locus_tag,genomic_pos,name,symbol,uniprot,refseq,taxid," +
                          "type_of_gene,genomic_pos_hg19,MGI,SGD,HGNC")
 
     def __init__(self, src_name=None, src_root_folder=None, no_confirm=True, archive=True):
         super().__init__(src_name, src_root_folder, no_confirm, archive)
         print(self.src_doc)
-        self.current_timestamp = self.src_doc["release"] if self.src_doc.get('release', False) else None
+        self.current_timestamp = datetime.strptime(self.src_doc["release"], "%Y%m%d") if self.src_doc.get('release', False) else None
         print(self.current_timestamp)
         self.new_timestamp = None
 
     def create_todump_list(self, force=False):
         if force or self.remote_is_newer():
-            self.release = datetime.now().strftime("%Y%m%d")
-            self.to_dump = [{"remote": 'http://mygene.info/v2/query/',
+            self.release = self.new_timestamp.strftime("%Y%m%d")
+            self.to_dump = [{"remote": 'http://mygene.info/v3/query/',
                              "local": os.path.join(self.new_data_folder, "mygene.json")}]
             self.logger.info(
                 "remote ({}) is newer than current ({})".format(self.new_timestamp, self.current_timestamp))
@@ -58,10 +41,28 @@ class MyGeneDumper(HTTPDumperParams):
                 "remote ({}) is not newer than current ({})".format(self.new_timestamp, self.current_timestamp))
 
     def remote_is_newer(self):
-        mygene_metadata = requests.get("http://mygene.info/v2/metadata").json()
-        self.new_timestamp = mygene_metadata['timestamp']
+        mygene_metadata = requests.get("http://mygene.info/v3/metadata").json()
+        self.new_timestamp = du.parse(mygene_metadata['timestamp'])
         if self.current_timestamp is None or self.new_timestamp > self.current_timestamp:
             return True
+
+    def download(self, remoteurl, localfile):
+        """
+        Use mygene client to do download
+        :param remoteurl:
+        :param localfile:
+        :return:
+        """
+        self.prepare_local_folders(localfile)
+        self.logger.debug("Downloading '%s'" % remoteurl)
+
+        mg = MyGeneInfo()
+        params = self.params
+        q = mg.query(params['q'], fields=params['fields'], species=params['species'],
+                     size=1000, fetch_all=True, entrezonly=True)
+        d = list(q)
+        with open(localfile, 'w') as f:
+            json.dump(d, f)
 
 
 def main():
--- wdbiothings/contrib/mygene/uploader.py
+++ wdbiothings/contrib/mygene/uploader.py
@@ -3,7 +3,7 @@ import os
 
 import biothings.dataload.uploader as uploader
 import requests
-
+from wdbiothings.local import JENKINS_TOKEN, JENKINS_URL
 
 class MyGeneUploader(uploader.BaseSourceUploader):
     name = "mygene"
@@ -12,8 +12,20 @@ class MyGeneUploader(uploader.BaseSourceUploader):
     def load_data(self, data_folder):
         with open(os.path.join(data_folder, "mygene.json")) as f:
             d = json.load(f)
-            for doc in d['hits']:
+            for doc in d:
                 yield doc
+
+    def post_update_data(self):
+        print("done uploading mygene")
+
+        params = {'token': JENKINS_TOKEN,
+                  'job': 'GeneBot_yeast'
+                  }
+        url = JENKINS_URL + "buildByToken/buildWithParameters"
+        r = requests.get(url, params=params)
+
+        # TODO add other jobs
+        print("job triggered")
 
     @classmethod
     def get_mapping(cls):
