651c11bca4fe9745d836698ca85c4c72d9e7a058
Greg Stupp <gstupp@scripps.edu>
2017-04-24 13:30:01 -0700

    add RGD,SGD,FLYBASE,WormBase,ZFIN

--- wdbiothings/contrib/mygene/dumper.py
+++ wdbiothings/contrib/mygene/dumper.py
@@ -25,13 +25,13 @@ class MyGeneDumper(HTTPDumper):
 
         df = get_ref_microbe_taxids()
         ref_taxids = list(map(str, df['taxid'].tolist()))
-        taxids = ['3702', '559292', '123', '10090', '9606', '10116', '243161',
+        taxids = ['3702', '559292', '123', '10090', '9606', '10116', '243161', '10116', '7227','6239','7955',
                   '515635', '765698', '525903', '759362', '565050', '446465'] + ref_taxids
         taxids = ",".join(set(taxids))
 
         self.params = dict(q="__all__", species=taxids, entrezonly="true", size="1000",
                       fields="entrezgene,ensembl,locus_tag,genomic_pos,name,symbol,uniprot,refseq,taxid," +
-                             "type_of_gene,genomic_pos_hg19,MGI,SGD,HGNC,homologene")
+                             "type_of_gene,genomic_pos_hg19,HGNC,homologene,MGI,RGD,SGD,FLYBASE,WormBase,ZFIN")
 
 
     def create_todump_list(self, force=False):
